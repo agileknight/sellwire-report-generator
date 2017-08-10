37705e25c6c88188d59bdaabc3ad0e4e27aa2bbc
Philipp Meier <philippmeier@Philipps-MacBook-Pro.local>
2017-08-10 02:13:12 +0200

    Add titles

--- pdf.sh
+++ pdf.sh
@@ -2,5 +2,5 @@
 
 docker build -t csv2pdf ./csv2pdf-docker
 
-docker run --rm -v $(pwd)/output:/output csv2pdf /csv2pdf --landscape --latex_encode --theme Zurich --in /output/Stripe.csv --out /output/Stripe.pdf
-docker run --rm -v $(pwd)/output:/output csv2pdf /csv2pdf --landscape --latex_encode --theme Zurich --in /output/Paypal.csv --out /output/Paypal.pdf
+docker run --rm -v $(pwd)/output:/output csv2pdf /csv2pdf --landscape --latex_encode --theme Zurich --title 'Stripe transactions' --in /output/Stripe.csv --out /output/Stripe.pdf
+docker run --rm -v $(pwd)/output:/output csv2pdf /csv2pdf --landscape --latex_encode --theme Zurich --title 'Paypal transactions' --in /output/Paypal.csv --out /output/Paypal.pdf
